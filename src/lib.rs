f15cdd14d2d03f2abf5b0fe2c08fd5339f28d554
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-14 18:24:10 +0530

    refactor: fibonacci series example

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,17 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+}
 
+fn fib(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+    fib(n - 1) + fib(n - 2)
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {name}"));
+pub fn greet(n: u32) {
+    let num = fib(n);
+    alert(&format!("Hello {n}:{num}"));
 }
